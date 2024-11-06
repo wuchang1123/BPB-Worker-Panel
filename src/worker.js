99cd530455cd79a8317dc707b59b9da90c008b4a
wuchang1123 <wuchang1123@gmail.com>
2024-11-06 20:53:01 +0800

    null Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -275,11 +275,13 @@ export default {
                         });
 
                     default:
+                    	const errorPage = renderErrorPage('Dataset is not properly set!', null, true);
+                    	return new Response(errorPage, { status: 200, headers: {'Content-Type': 'text/html'}});
                         // return new Response('Not found', { status: 404 });
-                        url.hostname = 'www.baidu.com';
-                        url.protocol = 'https:';
-                        request = new Request(url, request);
-                        return await fetch(request);
+                        //url.hostname = 'www.baidu.com';
+                        //url.protocol = 'https:';
+                        //request = new Request(url, request);
+                        //return await fetch(request);
                 }
             } else {
                 return url.pathname.startsWith('/tr') 
